f46862cfdb9d145ae838bc3869b35de8f93ef5a4
Begoña <Begoa@Lenovo-PC>
2023-03-23 22:28:01 +0100

    Modificado

---
+++ src/cuentas/CCuenta.java
@@ -0,0 +1,84 @@
+package cuentas;
+/**
+ * Esta tarea sirve para almacenar datos de la cuenta bancaria
+ * @author Begoña
+ */
+
+public class CCuenta {
+/**
+ * Para refactorizar vamos a refactor y en el desplegable seleccionamos 
+ * Encapsulate Fields
+ */
+/**
+ * Estos son los atributos de CCuenta
+ */
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
+    }
+
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+
+	private String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String getCuenta() {
+		return cuenta;
+	}
+
+	private void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	private double getSaldo() {
+		return saldo;
+	}
+
+	private void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	private double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	private void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
+}
+
